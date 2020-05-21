3059c093ed5abde827e04173760dcc00b736c62a
Sammie <43316541+ItsAMeSammieO@users.noreply.github.com>
2020-05-21 14:10:06 -0400

    Add files via upload
    Spell the word now has a multiplier function! Your points will now multiply for every question you get right!

--- spellTheWord.py
+++ spellTheWord.py
@@ -6,6 +6,8 @@ import playsound
 from gtts import gTTS
 
 yes = 'yes'
+score = 0
+scoreMultiplier = 1
 
 while yes == 'yes':
     letter = random.choice(string.ascii_lowercase)
@@ -21,14 +23,20 @@ while yes == 'yes':
     speak("Type this letter: " + letter)
     answer = str(input("What letter was said? "))
 
-
-
     if answer == letter:
         print("You typed the right letter!")
         speak("You typed the right letter!")
+        score += 1
+        scoreMultiplier += 1
+        print("You now have " + str(score * scoreMultiplier) + " points and your multiplier is now " + str(scoreMultiplier) + "!")
+        speak("You now have " + str(score * scoreMultiplier) + " points and your multiplier is now " + str(scoreMultiplier) + "!")
     elif answer != letter:
        print("That wasn't the right letter. The right letter was " + letter)
        speak("That wasn't the right letter. The right letter was " + letter)
+       score = 0
+       scoreMultiplier = 0
+       print("Your score has been reset to 0.")
+       speak("Your score has been reset to 0.")
 
     speak("Do you want to keep playing?")
     yes = input("Do you want to keep playing? ")
